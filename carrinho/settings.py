19bf815ca96e3832fa8fb4e5c6469e36bacbf529
Alexandre Sebrão <xandizitxu@iMac-de-Alexandre.local>
2016-06-19 17:10:07 -0300

    Stats

--- carrinho/settings.py
+++ carrinho/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'carrinho.middlewares.StatsMiddleware',
 ]
 
 ROOT_URLCONF = 'carrinho.urls'
