bd491218e7f484ba8f62085d84d3a84007b33987
Alexandre Sebrão <xandizitxu@iMac-de-Alexandre.local>
2016-06-19 19:45:54 -0300

    Preços e limpar carrinho

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,6 @@ app_name = 'pages'
 urlpatterns = [
     url(r'^addcarrinho/(?P<produto>[\w|\W]+)/', views.carrinho),
     url(r'^carrinho/', views.mostrar_carrinho),
+    url(r'^limpar_carrinho/', views.mostrar_carrinho),
     url(r'^$', views.index, name='index'),
 ]
