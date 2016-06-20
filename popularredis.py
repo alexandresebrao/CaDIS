a4ba34467bd141d9fe4b9d8d90c23c78f6d0196d
= <alexandre.sebrao@gmail.com>
2016-06-20 11:05:57 -0300

    Add popular redis

---
+++ popularredis.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import redis
+from cassandra.cluster import Cluster
+from cassandra.query import named_tuple_factory
+cluster = Cluster()
+session = cluster.connect('ecommerce')
+session.row_factory = named_tuple_factory
+produtos = session.execute('SELECT codigo, descricao, preco, quantidade FROM produto')
+r = redis.Redis(host='redis.kdalegends.me', port = 6379, password='aulaivo')
+
+# Criar key produto:"nome_do_produto" com valor "código_do_produto"
+for produto in produtos:
+	print "-----"
+	print produto.descricao
+	print produto.preco
+	print produto.codigo
+	print "-----"
+	string = "produto:%s" % (produto.descricao.replace(' ', '_'))
+	print string
+	print "-----"
+	# #string = "produto:%s" %produto['descricao']
+	# string = "produto:%s" %produto[1].replace (" ", "_")
+	# r.set(string, produto[0])
